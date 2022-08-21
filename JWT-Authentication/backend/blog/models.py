ac14b8ce43695c2539f4ec930f4c95b7f7d355e0
Matin.h.f81 <matinhosseini795@gmail.com>
2022-08-21 19:34:10 +0430

    create Article model and register it in admin

--- JWT-Authentication/backend/blog/models.py
+++ JWT-Authentication/backend/blog/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Article(models.Model):
+    title = models.CharField(max_length=255)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    body = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    status = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.title
